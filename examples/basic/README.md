d4ea5bb1923e95297c56319d2d48879ce04a3aa8
kalenarndt <kalenarndt@gmail.com>
2022-05-06 13:47:29 -0600

    refactor part 2

---
+++ examples/basic/README.md
@@ -0,0 +1,30 @@
+# basic
+
+<!-- BEGINNING OF PRE-COMMIT-TERRAFORM DOCS HOOK -->
+## Requirements
+
+No requirements.
+
+## Providers
+
+No providers.
+
+## Modules
+
+| Name | Source | Version |
+|------|--------|---------|
+| <a name="module_t1_basic"></a> [t1\_basic](#module\_t1\_basic) | ../../ | n/a |
+| <a name="module_t1_basic_no_downlink"></a> [t1\_basic\_no\_downlink](#module\_t1\_basic\_no\_downlink) | ../../ | n/a |
+
+## Resources
+
+No resources.
+
+## Inputs
+
+No inputs.
+
+## Outputs
+
+No outputs.
+<!-- END OF PRE-COMMIT-TERRAFORM DOCS HOOK -->
--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -1,36 +1,36 @@
 module "t1_basic" {
-  source = "../../"
-  tier0_gateway = "Provider-LR" # Provider Tier0 we will be downlinking to
-  t1_gateway = { # Map Variable we will iterate through to create the Tier 1 Gateways
-    t1_general = { # Name of the Tier 1 Gateway
-      description = "Tier1 Router that provides access for general workloads" # Description that will be applied
-      route_advertisement_types = ["TIER1_CONNECTED"] # The routes that we want to advertise to the Tier0
+  source        = "../../"
+  tier0_gateway = "Provider-LR"                                                             # Provider Tier0 we will be downlinking to
+  t1_gateway = {                                                                            # Map Variable we will iterate through to create the Tier 1 Gateways
+    t1_general = {                                                                          # Name of the Tier 1 Gateway
+      description               = "Tier1 Router that provides access for general workloads" # Description that will be applied
+      route_advertisement_types = ["TIER1_CONNECTED"]                                       # The routes that we want to advertise to the Tier0
     }
-    t1_dev = { # Name of the Tier 1 Gateway to be created
-      description = "Tier1 Router that provides access for developer workloads" # Description that will be applied
-      route_advertisement_types = ["TIER1_CONNECTED"] # The routes that we want to advertise to the Tier0
+    t1_dev = {                                                                                # Name of the Tier 1 Gateway to be created
+      description               = "Tier1 Router that provides access for developer workloads" # Description that will be applied
+      route_advertisement_types = ["TIER1_CONNECTED"]                                         # The routes that we want to advertise to the Tier0
     }
-    t1_prod = { # Name of the Tier 1 Gateway to be created
-      description = "Tier1 Router that provides access for developer workloads" # Description that will be applied
-      route_advertisement_types = ["TIER1_CONNECTED"] # The routes that we want to advertise to the Tier0
+    t1_prod = {                                                                               # Name of the Tier 1 Gateway to be created
+      description               = "Tier1 Router that provides access for developer workloads" # Description that will be applied
+      route_advertisement_types = ["TIER1_CONNECTED"]                                         # The routes that we want to advertise to the Tier0
     }
   }
 }
 
 module "t1_basic_no_downlink" {
   source = "../../"
-  t1_gateway = { # Map Variable we will iterate through to create the Tier 1 Gateways
-    t1_general = { # Name of the Tier 1 Gateway
-      description = "Tier1 Router that provides access for general workloads" # Description that will be applied
-      route_advertisement_types = ["TIER1_CONNECTED"] # The routes that we want to advertise
+  t1_gateway = {                                                                            # Map Variable we will iterate through to create the Tier 1 Gateways
+    t1_general = {                                                                          # Name of the Tier 1 Gateway
+      description               = "Tier1 Router that provides access for general workloads" # Description that will be applied
+      route_advertisement_types = ["TIER1_CONNECTED"]                                       # The routes that we want to advertise
     }
-    t1_dev = { # Name of the Tier 1 Gateway to be created
-      description = "Tier1 Router that provides access for developer workloads" # Description that will be applied
-      route_advertisement_types = ["TIER1_CONNECTED"] # The routes that we want to advertise
+    t1_dev = {                                                                                # Name of the Tier 1 Gateway to be created
+      description               = "Tier1 Router that provides access for developer workloads" # Description that will be applied
+      route_advertisement_types = ["TIER1_CONNECTED"]                                         # The routes that we want to advertise
     }
-    t1_prod = { # Name of the Tier 1 Gateway to be created
-      description = "Tier1 Router that provides access for developer workloads" # Description that will be applied
-      route_advertisement_types = ["TIER1_CONNECTED"] # The routes that we want to advertise
+    t1_prod = {                                                                               # Name of the Tier 1 Gateway to be created
+      description               = "Tier1 Router that provides access for developer workloads" # Description that will be applied
+      route_advertisement_types = ["TIER1_CONNECTED"]                                         # The routes that we want to advertise
     }
   }
 }
